2a3ae3bae2600528090d5bafe21ae959da36d7f0
SargisH2 <agentFreelance2@sykz.com>
2025-05-24 17:06:36 +0400

    fix for prod

--- admin_panel/app.js
+++ admin_panel/app.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("ws://localhost:8000/chat"); // local ws://localhost:8000/chat
+let ws = new WebSocket("wss://localhost:8000/chat"); // local ws://localhost:8000/chat
 
 function toISOStringLocal(dt) {
     if (!dt) return new Date().toISOString();
